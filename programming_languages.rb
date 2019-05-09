2d0b7e1fa98cf2f97130ccc1e9bdd92b0fcd54af
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-05-09 19:49:07 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   languages.each do |styles, info|
     info.each do |lang, details|
       details.each do |key, value|
-        puts key
+        puts lang
       end
     end
   end
